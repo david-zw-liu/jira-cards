ee7a452cc57fe6cea822423116b20dc00b442406
David Liu <msglight4874@gmail.com>
2020-07-28 17:17:49 +0800

    Updates

---
+++ precache-manifest.d0caea39c0a24d7359b8388d555034b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b61ee90c747d2a3e5944880b9df3510",
+    "url": "/jira-cards/index.html"
+  },
+  {
+    "revision": "d6d84907789cc973980e",
+    "url": "/jira-cards/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "41c067355458010d17a3",
+    "url": "/jira-cards/static/js/2.05899b36.chunk.js"
+  },
+  {
+    "revision": "12ac8af0741afe107c3dcb4ef10e1ce5",
+    "url": "/jira-cards/static/js/2.05899b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d84907789cc973980e",
+    "url": "/jira-cards/static/js/main.feaba83c.chunk.js"
+  },
+  {
+    "revision": "5c4dec18926f07140c73",
+    "url": "/jira-cards/static/js/runtime-main.2a89af7b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da0cee18f56d88c381b3422f0b18ff8f.js"
+  "/jira-cards/precache-manifest.d0caea39c0a24d7359b8388d555034b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jira-cards/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
